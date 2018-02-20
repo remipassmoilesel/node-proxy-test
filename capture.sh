0a7339bc6d940e4e40228297225c9b9d47528085
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2018-02-21 00:36:16 +0100

    Add capture example

---
+++ capture.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# mercredi 21 février 2018, 00:32:20 (UTC+0100)
+
+# See: https://github.com/socsieng/capture-proxy
+
+# npm i -g capture-proxy
+
+capture http://www.google.com/ -p 3000 -R -o ./captures
