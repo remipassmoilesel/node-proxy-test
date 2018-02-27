b7fb21cf0fae240b7fc028edda82e21e1c6aaf02
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2018-02-27 18:18:09 +0100

    Add first try on test play

--- src/common/testUtils.ts
+++ src/common/testUtils.ts
@@ -1,11 +1,28 @@
+import * as chai from "chai";
 import { wait } from "f-promise";
 import * as got from "got";
 import { HttpRequest } from "../proxy-server/HttpRequest";
+import { printInfo } from "./common";
+
+const assert = chai.assert;
 
 export function runRequest(request: HttpRequest) {
-    const res = wait(got(request.url, {
-        headers: request.headers,
-    }));
-    return res;
+
+    printInfo(`Running request: ${request.url}`);
+
+    let response;
+    try {
+        response = wait(got(request.url, {
+            headers: request.headers,
+        }));
+    } catch (e) {
+        response = e;
+    }
+
+    if (request.expectedResponse && request.expectedResponse.code) {
+        assert.equal(request.expectedResponse.code, response.statusCode);
+    }
+
+    return response;
 }
 
--- src/templates/TemplateSpec.ts
+++ src/templates/TemplateSpec.ts
@@ -1,14 +1,18 @@
 // tslint:disable
 import * as chai from "chai";
+import * as fmocha from "f-mocha";
 /*<#requestsImports>*/
 /*<&importLine>*/
 /*</requestsImports>*/
+
+fmocha.setup();
 
 const assert = chai.assert;
 
 /*<&requestsInstantiation>*/
 
-describe(" > Some interesting module", () => {
+describe(" > Some interesting module", function() {
+    this.timeout(10000);
 
     it(" > All requests", () => {
         // Example:
