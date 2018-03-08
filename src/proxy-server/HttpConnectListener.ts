300fe1c09083609be3960ab7124c1782db231454
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2018-03-08 11:38:09 +0100

    Lint fix + improve display

--- src/proxy-server/HttpConnectListener.ts
+++ src/proxy-server/HttpConnectListener.ts
@@ -1,10 +1,10 @@
-import * as http from "http";
-import * as net from "net";
-import {printColor, printWarning} from "../common/print";
+import * as http from 'http';
+import * as net from 'net';
+import { printColor, printWarning } from '../common/print';
 
 const debugging = false;
 
-const regex_hostport = /^([^:]+)(:([0-9]+))?$/;
+const regexHostPort = /^([^:]+)(:([0-9]+))?$/;
 
 // see https://newspaint.wordpress.com/2012/11/05/node-js-http-and-https-proxy/
 
@@ -19,95 +19,87 @@ export class HttpConnectListener {
         // add handler for HTTPS (which issues a CONNECT to the proxy)
         server.addListener('connect', (request, socketRequest, bodyhead) => {
 
-                printWarning('HTTPS upgrade: HTTPS requests cannot be recorded !');
-
-                const url = request['url'];
-                const httpVersion = request['httpVersion'];
-
+                const url = request.url;
+                const httpVersion = request.httpVersion;
                 const hostport = this.getHostPortFromString(url, '443');
 
-                if (debugging)
+                printWarning(`HTTPS upgrade (HTTPS requests cannot be recorded): ${url}`);
+
+                if (debugging) {
                     log('  = will connect to %s:%s', hostport[0], hostport[1]);
+                }
 
                 // set up TCP connection
                 const proxySocket = new net.Socket();
-                proxySocket.connect(
-                    parseInt(hostport[1]), hostport[0],
-                    function () {
-                        if (debugging)
+                proxySocket.connect(parseInt(hostport[1]), hostport[0], () => {
+                        if (debugging) {
                             log('  < connected to %s/%s', hostport[0], hostport[1]);
+                        }
 
-                        if (debugging)
+                        if (debugging) {
                             log('  > writing head of length %d', bodyhead.length);
+                        }
 
                         proxySocket.write(bodyhead);
 
                         // tell the caller the connection was successfully established
-                        socketRequest.write("HTTP/" + httpVersion + " 200 Connection established\r\n\r\n");
-                    }
+                        socketRequest.write('HTTP/' + httpVersion + ' 200 Connection established\r\n\r\n');
+                    },
                 );
 
-                proxySocket.on(
-                    'data',
-                    function (chunk: any) {
-                        if (debugging)
+                proxySocket.on('data', (chunk: any) => {
+                        if (debugging) {
                             log('  < data length = %d', chunk.length);
+                        }
 
                         socketRequest.write(chunk);
-                    }
+                    },
                 );
 
-                proxySocket.on(
-                    'end',
-                    function () {
-                        if (debugging)
+                proxySocket.on('end', () => {
+                        if (debugging) {
                             log('  < end');
+                        }
 
                         socketRequest.end();
-                    }
+                    },
                 );
 
-                socketRequest.on(
-                    'data',
-                    function (chunk: any) {
-                        if (debugging)
+                socketRequest.on('data', (chunk: any) => {
+                        if (debugging) {
                             log('  > data length = %d', chunk.length);
+                        }
 
                         proxySocket.write(chunk);
-                    }
+                    },
                 );
 
-                socketRequest.on(
-                    'end',
-                    function () {
-                        if (debugging)
+                socketRequest.on('end', () => {
+                        if (debugging) {
                             log('  > end');
+                        }
 
                         proxySocket.end();
-                    }
+                    },
                 );
 
-                proxySocket.on(
-                    'error',
-                    function (err) {
-                        socketRequest.write("HTTP/" + httpVersion + " 500 Connection error\r\n\r\n");
+                proxySocket.on('error', (err) => {
+                        socketRequest.write('HTTP/' + httpVersion + ' 500 Connection error\r\n\r\n');
                         if (debugging) {
                             log('  < ERR: %s', err);
                         }
                         socketRequest.end();
-                    }
+                    },
                 );
 
-                socketRequest.on(
-                    'error',
-                    function (err: any) {
+                socketRequest.on('error', (err: any) => {
                         if (debugging) {
                             log('  > ERR: %s', err);
                         }
                         proxySocket.end();
-                    }
+                    },
                 );
-            }
+            },
         ); // HTTPS connect listener
     }
 
@@ -115,7 +107,7 @@ export class HttpConnectListener {
         let host = hostString;
         let port = defaultPort;
 
-        const result = regex_hostport.exec(hostString);
+        const result = regexHostPort.exec(hostString);
         if (result != null) {
             host = result[1];
             if (result[2] != null) {
